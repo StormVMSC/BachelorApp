5037962889b2e8c80b4dce6935714b623f2340ad
kumia01 <thien11hei@gmail.com>
2023-05-01 15:15:22 +0200

    implementert hente funksjon for config

--- src/main/resources/static/config.js
+++ src/main/resources/static/config.js
@@ -1,7 +1,7 @@
 
-function getInventoryList(){
+function getHostList(){
     $.ajax({
-        url: "/GetInventory",
+        url: "/GetHostList",
         method: "GET",
         success: function(data) {
             // Handle successful response
@@ -17,11 +17,60 @@ function getInventoryList(){
 }
 
 function formatHostList(data){
-    let ut = "<select class='select' data-mdb-filter='true'>";
-    for(let inventory of data){
-        ut +=
-            "<option value="+ inventory.id +">" + inventory.name + "</option>"
+    let ut = "";
+    for(let host of data){
+        ut +="<div class='col-sm-3'>\n" +
+            "    <div class='card_area'>\n" +
+            "        <input class='checkbox' type='checkbox'/>\n" +
+            "        <div class='single_card'>\n" +
+            "            <div class='card_body cluster1'>\n" +
+            "                <div class='text-center'><img src='img/databaser.png' class='box-icon'></div>\n" +
+            "                <div class='card_title'>\n" +
+            "                    <h3>"+ host.name +"</h3>\n" +
+            "                </div>\n" +
+            "            </div>\n" +
+            "        </div>\n" +
+            "    </div>\n" +
+            "</div>"
+            ;
     }
-    ut += "</select>";
-    $("#inventory").html(ut);
+    $("#host").html(ut);
+}
+
+function getPlaybookList(){
+    $.ajax({
+        url: "/GetPlaybooks",
+        method: "GET",
+        success: function(data) {
+            // Handle successful response
+            console.log(data); // This will print the response to the console
+            data.sort((a, b) => a.id - b.id);
+            formatPlaybookList(data);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            // Handle error response
+            console.error(errorThrown); // This will print the error to the console
+        }
+    });
+}
+
+function formatPlaybookList(data){
+    let ut = "";
+    for( let playbook of data){
+        ut +="<div class='col-sm-3'>\n" +
+            "    <div class='card_area'>\n" +
+            "        <input class='checkbox' type='checkbox'/>\n" +
+            "        <div class='single_card'>\n" +
+            "            <div class='card_body cluster1'>\n" +
+            "                <div class='text-center'><img src='img/databaser.png' class='box-icon'></div>\n" +
+            "                <div class='card_title'>\n" +
+            "                    <h3>"+ playbook.navn +"</h3>\n" +
+            "                </div>\n" +
+            "            </div>\n" +
+            "        </div>\n" +
+            "    </div>\n" +
+            "</div>"
+            ;
+    }
+    $("#playbook").html(ut);
 }
