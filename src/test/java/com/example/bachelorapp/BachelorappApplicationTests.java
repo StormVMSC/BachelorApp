80adf7681a9650d02ab6e68cbeb434bef4253a93
Spermulf <gjermundhertzberg@gmail.com>
2023-05-02 18:57:19 +0200

    Lagt til noen feilhåndteringer

--- src/test/java/com/example/bachelorapp/BachelorappApplicationTests.java
+++ src/test/java/com/example/bachelorapp/BachelorappApplicationTests.java
@@ -1,10 +1,16 @@
 package com.example.bachelorapp;
 
+import jakarta.annotation.security.RunAs;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 class BachelorappApplicationTests {
+
+    @Autowired
+    private AnsibleAPIRepository APIRepository;
 
     @Test
     void contextLoads() {
